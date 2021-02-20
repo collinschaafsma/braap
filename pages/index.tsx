adf0da99e68f30913387e95e23352d93e2465615
Collin Schaafsma <schaafsma@gmail.com>
2021-02-20 13:49:33 -0700

    adding fauna

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,49 @@
 import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import Layout from '../components/Layout'
+import { graphQLClient } from '../utils/graphql-client'
+import { gql } from 'graphql-request'
+import useSWR from 'swr'
+import { ToDouble } from 'faunadb'
+
+const fetcher = async (query) => await graphQLClient.request(query);
 
 export default function Home() {
+
+  const { error, data } = useSWR(
+    gql`
+        {
+        videos {
+          data {
+            _id
+            link
+            _ts
+          }
+        }
+      }
+    `,
+    fetcher  
+  )
+  
+  if (error) {
+    console.log(error)
+    return <div>borked</div>
+  }
+  console.log(data)
+
+
   return (
     <Layout>
       <Head>
-        <title>Hello-Home</title>
+        <title>Braap</title>
       </Head>
-      <div className="bg-gradient-to-r from-red-500 shadow">index</div>
+      <div className="bg-gradient-to-r from-red-500 shadow">Videos
+        <ul>
+          {data.videos.data.map((video) => (
+            <li>{video.link}</li>
+          ))}
+        </ul>
+      </div>
     </Layout>
   )
 }
